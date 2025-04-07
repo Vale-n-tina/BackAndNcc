24c60fba2868888486afde4748f9a03d4495eb90
Vale-n-tina <valentia.grg@gmail.com>
2025-04-07 19:26:30 +0200

    .

--- src/main/java/it/ncc/BackAndNcc/auth/AuthRunner.java
+++ src/main/java/it/ncc/BackAndNcc/auth/AuthRunner.java
@@ -21,10 +21,10 @@ public class AuthRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // Creazione dell'utente admin se non esiste
-//        Optional<AppUser> adminUser = appUserService.findByUsername("admin");
-//        if (adminUser.isEmpty()) {
-//            appUserService.registerUser("username", "password", Set.of(Role.ROLE_ADMIN));
-//        }
+        Optional<AppUser> adminUser = appUserService.findByUsername("admin");
+        if (adminUser.isEmpty()) {
+            appUserService.registerUser(".", ".", Set.of(Role.ROLE_ADMIN));
+        }
 
 
     }
